e981cf56ca1186920c9e8b2aa0f5431f0174c54a
Brandon Walden <brandonshanewalden@gmail.com>
2017-08-19 10:18:17 -0400

    added inserts to vending machines SQL

---
+++ vending_machine/app.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const {
+  Client
+} = require('pg');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+const client = new Client({
+  user: 'brandonwalden',
+  host: 'localhost',
+  database: 'vendingMachine',
+  password: '',
+  port: 5432
+});
+client.connect();
+
+app.get('/', function(request, response) {
+  response.send('ok so you want a snack');
+});
+
+app.get('/machines', function(request, response) {
+  client.query('SELECT * FROM machine',
+    function(request, dbResponse) {
+      response.json({
+        machines: dbResponse.rows
+      })
+    });
+});
+
+app.get('/machines/:machine_id', function(request, response) {
+  client.query('SELECT * FROM machine WHERE machine_id =$1', [request.params.machine_id],
+    function(request, dbResponse) {
+      response.json({
+        machine: dbResponse.rows[0]
+      })
+    });
+});
+
+app.get('/machines/:machine_id/snacks', function(request, response) {
+  client.query('SELECT * FROM item WHERE machine_id =$1', [request.params.machine_id],
+    function(request, dbResponse) {
+      response.json({
+        snacks: dbResponse.rows
+      })
+    });
+});
+
+app.get('/machines/:machine_id/purchaseHistory', function(request, response) {
+  client.query('SELECT * FROM purchase WHERE machine_id =$1', [request.params.machine_id],
+    function(request, dbResponse) {
+      response.json({
+        purchases: dbResponse.rows
+      })
+    });
+});
+
+app.post('/machines/:machine_id/snackTime', function(request, response) {
+  var date = new Date();
+  var payment = request.body.payment;
+  var machine_id = request.params.machine_id;
+  var item_id = request.body.item_id;
+
+  client.query("SELECT cost from item WHERE item_id =$1", [request.body.item_id], function(request, dbResponse) {
+    var price = dbResponse.rows[0].cost;
+    var change = payment - price;
+    console.log(price);
+    console.log(payment);
+    console.log(change);
+    if (payment < price) {
+      response.json({
+        status: 'Failed',
+        money_given: payment,
+        cost: price
+      })
+
+    } else {
+      client.query('INSERT into purchase(purchase_time,machine_id,item_id, payment,change) VALUES($1, $2, $3, $4, $5)', [date, machine_id, item_id, payment, change],
+        function(err, dbResponse) {
+
+          response.json({
+            status: 'ok'
+          })
+        })
+    }
+  })
+
+});
+
+app.listen(3000, function() {
+  console.log('Vending on port 3000...');
+});
---
+++ vending_machine/setup.sql
@@ -0,0 +1,57 @@
+DROP DATABASE IF EXISTS vendingMachine;
+CREATE DATABASE vendingMachine;
+
+\c vendingMachine;
+
+CREATE TABLE machine(
+  machine_id SERIAL PRIMARY KEY,
+  bank FLOAT,
+  location TEXT
+);
+
+CREATE TABLE item(
+  item_id SERIAL PRIMARY KEY,
+  cost FLOAT,
+  name TEXT,
+  amount INTEGER
+  machine_id INTEGER REFERENCES machine
+);
+
+
+CREATE TABLE purchase(
+  purchase_id SERIAL PRIMARY KEY,
+  purchase_time TIMESTAMP,
+  machine_id INTEGER REFERENCES machine,
+  item_id INTEGER REFERENCES item,
+  payment FLOAT,
+  change FLOAT
+);
+
+INSERT INTO machine (bank, location)
+VALUES(200.00, '1st Floor'),
+      (100.00, '2nd Floor'),
+      (50.50, '3rd Floor');
+
+INSERT INTO item (cost, name, amount, machine_id)
+VALUES
+  (1.00,'Reeses Cups King Size',12,(SELECT machine_id FROM machine WHERE location='1st Floor')),
+  (0.75,'Skittles',10,(SELECT machine_id FROM machine WHERE location ='1st Floor')),
+  (0.75,'Nutri Bar Apple',6,(SELECT machine_id FROM machine WHERE location ='1st Floor')),
+  (1.00,'Lays Chips',10,(SELECT machine_id FROM machine WHERE location='2nd Floor')),
+  (1.00,'7UP',8,(SELECT machine_id FROM machine WHERE location='2nd Floor')),
+  (0.25,'Wrigleys Gum',12,(SELECT machine_id FROM machine WHERE location='2nd Floor')),
+  (1.00,'Sprite',7,(SELECT machine_id FROM machine WHERE location='3rd Floor')),
+  (1.00,'Coca-Cola',5,(SELECT machine_id FROM machine WHERE location='3rd Floor')),
+  (1.00,'Smokes',10,(SELECT machine_id FROM machine WHERE location='3rd Floor'));
+  
+
+
+
+INSERT INTO purchase
+VALUES('2017-08-17 09:00:00',(SELECT machine_id FROM machine WHERE location='2nd Floor'),(SELECT item_id FROM item WHERE name='Doritos'), 5.00, 4.00),
+('2017-08-17 13:45:00',(SELECT machine_id FROM machine WHERE location='3rd Floor'),(SELECT item_id FROM item WHERE name='CocaCola'),1.00,0),
+('2017-08-16 12:15:00',(SELECT machine_id FROM machine WHERE location='1st Floor'),(SELECT item_id FROM item WHERE name='Jack Daniels'), 10.00, 4.00);
+
+SELECT * FROM machine;
+SELECT * FROM item;
+SELECT * FROM purchase;
